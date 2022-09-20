a67e84a49fe8ce46b441272897af5312b9df6f2a
Ameer58 <aimam58@gmail.com>
2022-09-20 11:56:07 +0100

    unitTesting

---
+++ unitTestLesson/unitTestLessonApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace unitTestLessonApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
