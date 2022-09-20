77832e9dc6f38647b5eb77b4aabe9840ceaadca8
Ameer58 <aimam58@gmail.com>
2022-09-20 12:19:00 +0100

    refactored

--- unitTestLesson/unitTestLessonApp/Program.cs
+++ unitTestLesson/unitTestLessonApp/Program.cs
@@ -6,7 +6,26 @@ namespace unitTestLessonApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int timeOfDay = 21;
+            string output = GetGreeting(timeOfDay);
+            Console.WriteLine(output);
+        }
+
+        private static string GetGreeting(int timeOfDay)
+        {
+            
+            if (timeOfDay >= 5 && timeOfDay <= 12)
+            {
+                return "Good morning!";
+            }
+            else if (timeOfDay >= 12 && timeOfDay <= 18)
+            {
+                return"Good afternoon!";
+            }
+            else
+            {
+                return"Good evening!";
+            }
         }
     }
 }
