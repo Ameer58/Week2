df77afddd7152d288b9bf7a538e663be9ccd1bf9
Ameer58 <aimam58@gmail.com>
2022-09-21 10:24:30 +0100

    Initial

---
+++ operatorsAndControlApp/operatorsAndControlApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace operatorsAndControlApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
