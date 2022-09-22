d72ad0a5d643ea1b03aafee15bea24d8049b7a10
Ameer58 <aimam58@gmail.com>
2022-09-22 16:21:31 +0100

    Completed Control FLow lab

---
+++ Operators_ControlFlow_Lab_Starter/Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Lib/Exercises.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections.Generic;
+
+namespace Op_CtrlFlow
+{
+    public class Exercises
+    {
+        public static bool MyMethod(int num1, int num2)
+        {
+            return num1 == num2 ? false : (num1 % num2) == 0;
+        }
+
+        // returns the average of the array nums
+        public static double Average(List<int> nums)
+        {
+            double sum = 0;
+            if(nums.Count == 0) return 0;
+            for(int i = 0; i < nums.Count; i++)
+            {
+                sum += nums[i];
+            }
+            double average = sum / nums.Count;
+            return average;
+        }
+
+        // returns the type of ticket a customer is eligible for based on their age
+        // "Standard" if they are between 18 and 59 inclusive
+        // "OAP" if they are 60 or over
+        // "Student" if they are 13-17 inclusive
+        // "Child" if they are 5-12
+        // "Free" if they are under 5
+        public static string TicketType(int age)
+        {
+            string ticketType = string.Empty;
+            if (age < 5) ticketType = "Free";
+            else if (age <= 12) ticketType = "Child";
+            else if (age <= 17) ticketType = "Student";
+            else if (age <= 59) ticketType = "Standard";
+            else ticketType = "OAP";
+
+            return ticketType;
+        }
+
+        public static string Grade(int mark)
+        {
+            var grade = "";
+            if (mark > 74)
+                grade = "Pass with Distinction";
+            else if (mark > 59)
+                grade = "Pass with Merit";
+            else if (mark > 39)
+                grade = "Pass";
+            else
+                grade = "Fail";
+            return grade;
+        }
+
+        public static int GetScottishMaxWeddingNumbers(int covidLevel)
+        {
+            int attendes = 0;
+            switch (covidLevel)
+            {
+                case 0:
+                    attendes = 200;
+                    break;
+                case 1:
+                    attendes = 100;
+                    break;
+                case 2:
+                    attendes = 50;
+                    break;
+                case 3:
+                    attendes = 50;
+                    break;
+                case 4:
+                    attendes = 20;
+                    break;
+
+
+            }
+            return attendes;
+        }
+    }
+}
---
+++ Operators_ControlFlow_Lab_Starter/Operators_ControlFlow_Lab_Starter/Op_CtrlFlow_Tests/Exercises_Tests.cs
@@ -0,0 +1,139 @@
+﻿using NUnit.Framework;
+using Op_CtrlFlow;
+using System;
+using System.Collections.Generic;
+
+namespace Op_CtrlFlow_Tests
+{
+    public class Exercises_Tests
+    {
+        // write unit test(s) for MyMethod here
+        [Test]
+        [TestCase(2,2)]
+        [TestCase(3,2)]
+        [TestCase(7, 2)]
+        public void ReturnFalseIfNumbersAreNotEqualAnd1StNumberIsntMultipleof2nd(int x, int y)
+        {
+            Boolean output;
+            output = Exercises.MyMethod(x, y);
+            Assert.That(output, Is.EqualTo(false));
+        }
+
+        [Test]
+        [TestCase(6,2)]
+        [TestCase(4, 2)]
+        [TestCase(14, 7)]
+        public void ReturnTrueIf1stNumberIsMultipleOf2nd(int x, int y)
+        {
+            Boolean output;
+            output = Exercises.MyMethod(x, y);
+            Assert.That(output, Is.EqualTo(true));
+        }
+
+        [Test]
+        public void Average_ReturnsCorrectAverage()
+        {
+            var myList = new List<int>() { 3, 8, 1, 7, 3 };
+            Assert.That(Exercises.Average(myList), Is.EqualTo(4.4));
+        }
+
+        [Test]
+        public void WhenListIsEmpty_Average_ReturnsZero()
+        {
+            var myList = new List<int>() {};
+            Assert.That(Exercises.Average(myList), Is.EqualTo(0));
+        }
+
+        [TestCase(100, "OAP")]
+        [TestCase(60, "OAP")]
+        [TestCase(59, "Standard")]
+        [TestCase(18, "Standard")]
+        [TestCase(17, "Student")]
+        [TestCase(13, "Student")]
+        [TestCase(12, "Child")]
+        [TestCase(5, "Child")]
+        [TestCase(4, "Free")]
+        [TestCase(0, "Free")]
+        public void TicketTypeTest(int age, string expected)
+        {
+            var result = Exercises.TicketType(age);
+            Assert.That(result, Is.EqualTo(expected));
+        }
+        [Test]
+        [TestCase(75)]
+        [TestCase(90)]
+        [TestCase(100)]
+        public void GradeBetween75And100_ReturnsDistinction(int mark)
+        {
+            string grade = Exercises.Grade(mark);
+            Assert.That(grade, Is.EqualTo("Pass with Distinction"));
+        }
+        [Test]
+        [TestCase(74)]
+        [TestCase(60)]
+        [TestCase(70)]
+        public void GradeBetween60And74_ReturnsMerit(int mark)
+        {
+            string grade = Exercises.Grade(mark);
+            Assert.That(grade, Is.EqualTo("Pass with Merit"));
+        }
+        [Test]
+        [TestCase(59)]
+        [TestCase(49)]
+        [TestCase(40)]
+        public void GradeBetween40And59_ReturnsMerit(int mark)
+        {
+            string grade = Exercises.Grade(mark);
+            Assert.That(grade, Is.EqualTo("Pass"));
+        }
+        [Test]
+        [TestCase(39)]
+        [TestCase(10)]
+        [TestCase(0)]
+        public void GradeBelow40_ReturnsFail(int mark)
+        {
+            string grade = Exercises.Grade(mark);
+            Assert.That(grade, Is.EqualTo("Fail"));
+        }
+
+        [Test]
+        [TestCase(0)]
+        public void AlertLevel0Attendes(int level)
+        {
+            int attendes = Exercises.GetScottishMaxWeddingNumbers(level);
+            Assert.That(attendes, Is.EqualTo(200));
+        }
+
+        [Test]
+        [TestCase(1)]
+        public void AlertLevel1Attendes(int level)
+        {
+            int attendes = Exercises.GetScottishMaxWeddingNumbers(level);
+            Assert.That(attendes, Is.EqualTo(100));
+        }
+
+        [Test]
+        [TestCase(2)]
+        public void AlertLevel2Attendes(int level)
+        {
+            int attendes = Exercises.GetScottishMaxWeddingNumbers(level);
+            Assert.That(attendes, Is.EqualTo(50));
+        }
+
+        [Test]
+        [TestCase(3)]
+        public void AlertLevel3Attendes(int level)
+        {
+            int attendes = Exercises.GetScottishMaxWeddingNumbers(level);
+            Assert.That(attendes, Is.EqualTo(50));
+        }
+
+        [Test]
+        [TestCase(4)]
+        public void AlertLevel4Attendes(int level)
+        {
+            int attendes = Exercises.GetScottishMaxWeddingNumbers(level);
+            Assert.That(attendes, Is.EqualTo(20));
+        }
+    }
+}
