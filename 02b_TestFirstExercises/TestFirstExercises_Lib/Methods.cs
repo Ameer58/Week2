ca41d27b079f57a68ecf82c651cf14ee73aad3f5
Ameer58 <aimam58@gmail.com>
2022-09-22 15:18:38 +0100

    Completed TestFirstExercises

--- 02b_TestFirstExercises/TestFirstExercises_Lib/Methods.cs
+++ 02b_TestFirstExercises/TestFirstExercises_Lib/Methods.cs
@@ -7,7 +7,7 @@ namespace TestFirstExercises
     {
         // implement this method so that it returns true if x is greater than or equal to y
         public static bool GreaterEqual(int x, int y)
-        {
+        {   if (x >= y) return true; else
             return false;
         }
 
@@ -16,13 +16,18 @@ namespace TestFirstExercises
         // Return a double rounded to 3 decimal places.
         public static double BODMAS(int inputNumber)
         {
-            return 0;
+            double x = inputNumber* inputNumber;
+            x += 101;
+            x = x / 7 - 4;
+            x = Math.Round(x, 3);
+            return x;
         }
 
         // implement this method so that it returns true if num is even, otherwise false
         public static bool EvenOdd(int num)
         {
-            return false;
+            if (num %2 == 0) return true;
+            else return false; ;
         }
 
         // implement this method so that it returns 
@@ -30,21 +35,39 @@ namespace TestFirstExercises
         // that are divisible by either 2 or 5
         public static int SumEvenFive(int max)
         {
-            return -1;
+            int sum = 0;  
+            for (int i = 1; i <= max; i++)
+            {
+                
+                if (i % 2 == 0 || i%5==0)
+                {
+                    sum += i;
+                }
+                
+            }
+            return sum;
         }
 
         // implement this method so it returns true if input is "password"
         // regardless of case
         public static bool CheckInput(string input)
         {
-            return false;
+            string word = "password";
+            string i = input.ToLower();
+            if(i.Equals(word)) return true;
+            else return false;
         }
 
         // implement this method so it returns the sum
         // of all the numbers in the list
         public static int SumList(List<int> list)
         {
-            return -1;
+            int sum = 0;
+            for(int i =0; i< list.Count; i++)
+            {
+                sum += list[i];
+            }
+            return sum;
         }
     }
 }
