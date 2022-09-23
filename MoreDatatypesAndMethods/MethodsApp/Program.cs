89ed1d723849b6406f38b6b1057a3f255788a43a
Ameer58 <aimam58@gmail.com>
2022-09-23 16:04:00 +0100

    Initial commit

---
+++ MoreDatatypesAndMethods/MethodsApp/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace MethodsApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var result = DoThis(10, "Enthused");
+            var result2 = DoThis(25);
+            var pizza1 = OrderPizza(true, false, true);
+
+            var myTuple = ("Joe", "Bloggs", 25);
+            Console.WriteLine(myTuple);
+            Console.WriteLine(myTuple.Item2);
+        }
+
+        public static int DoThis(int x, string y = "Happy")
+        {
+            Console.WriteLine($"I'm feeling {y}");
+            return x * x;
+        }
+
+        public static string OrderPizza(bool anchovies, bool pineapple, bool mushrooms = false)
+        {
+            var pizza = "Pizza with tomato sauce, cheese, ";
+            if (anchovies) pizza += "anchovies, ";
+            if (anchovies) pizza += "pineapple, ";
+            if (anchovies) pizza += "mushrooms, ";
+            return pizza;
+        }
+    }
+}
--- MoreDatatypesAndMethods/MoreDatatypesApp/Program.cs
+++ MoreDatatypesAndMethods/MoreDatatypesApp/Program.cs
@@ -1,10 +1,59 @@
-﻿namespace MoreDatatypesApp
+﻿using System.Text;
+using static System.Formats.Asn1.AsnWriter;
+
+namespace MoreDatatypesApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            // var myString = "  C# list fundamentals ";
+            //Console.WriteLine(StringBuilderExercise(myString))
+            int scoreA = 2;
+            int scoreB = 1; 
+            string concatString1 = "Liverpool : " + scoreA + ", Manchester United : " + scoreB;
+
+            string concatString2 = String.Concat("Liverpool : ", scoreA, ", Manchester United : ", scoreB);
+
+            StringInterpolation("Cathy");
+           
+        
+        }
+        public static void StringInterpolation(string str)
+        {
+            Console.WriteLine("My name is : " + str + " using +");
+            Console.WriteLine($"My name is: {str} using interpolation");
+            var num1 = 2;
+            var num2 = 7;
+            var fString = $"{num1} to the power of {num2} is {Math.Pow(num1, num2)}";
+            Console.WriteLine(fString);
+
+            var fString2 = $"Log to the base {num2} of {num1} is {Math.Log(num2, num1):0.####}";
+            var fString3 = $"That will be {num2 / 3.0:c} please!";
+            ParsingStrings();
+        }
+
+        public static string StringExercise(string myString)
+        {
+            string iString = myString.Trim().ToUpper().Replace('L', '*').Replace('T', '*');
+            var x = iString.IndexOf('N');
+            return iString.Remove(x + 1);
+        }    
+        public static string StringBuilderExercise(string myString)
+        {
+            StringBuilder sb = new StringBuilder(myString);
+
+            sb.Replace('l', '*').Replace('t', '*').Remove(myString.IndexOf('n')+1, sb.Length - myString.IndexOf('n')-1);
+            return sb.ToString().Trim().ToUpper();
+
+        }
+
+        public static void ParsingStrings()
+        {
+            // parsing strings    Console.WriteLine("How many apples?");
+            Console.WriteLine("How many apples?");
+            string input = Console.ReadLine();
+            var succes = Int32.TryParse(input, out int parsedApples);
         }
     }
 }
