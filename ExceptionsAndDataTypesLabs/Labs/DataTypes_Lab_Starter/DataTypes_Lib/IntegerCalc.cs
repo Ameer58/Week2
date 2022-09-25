4982c68b016fd44c8b37d8cd02aeef48a3c4e8a1
Ameer58 <aimam58@gmail.com>
2022-09-25 22:48:42 +0100

    Completed lab

---
+++ ExceptionsAndDataTypesLabs/Labs/DataTypes_Lab_Starter/DataTypes_Lib/IntegerCalc.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DataTypes_Lib
+{
+    public static class IntegerCalc
+    {
+        public static int Add(int num1, int num2)
+        {
+            if ((num1 >= int.MaxValue | num1 <= int.MinValue) | (num2 >= int.MaxValue | num2 <= int.MinValue)) throw new OverflowException();
+             return num1 + num2;
+        }
+
+        public static int Subtract(int num1, int num2)
+        {
+            if ((num1 >= int.MaxValue | num1 <= int.MinValue) | (num2 >= int.MaxValue | num2 <= int.MinValue)) throw new OverflowException();
+            return num1 - num2;
+        }
+
+        public static int Multiply(int num1, int num2)
+        {
+            if ((num1 >= int.MaxValue | num1 <= int.MinValue) | (num2 >= int.MaxValue | num2 <= int.MinValue)) throw new OverflowException();
+            return num1 * num2;
+        }
+
+        public static int Divide(int num1, int num2)
+        {
+            if (num1 == 0 | num2 == 0) throw new ArgumentException("Can't divide by zero");
+            if ((num1 >= int.MaxValue | num1 <= int.MinValue) | (num2 >= int.MaxValue | num2 <= int.MinValue)) throw new OverflowException();
+            return num1 / num2;
+        }
+
+        public static int Modulus(int num1, int num2)
+        {
+            if (num1 == 0 | num2 == 0) throw new ArgumentException("Can't modulo by zero");
+            if ((num1 >= int.MaxValue | num1 <= int.MinValue) | (num2 >= int.MaxValue | num2 <= int.MinValue)) throw new OverflowException();
+            return num1 % num2;
+        }
+    }
+}
---
+++ ExceptionsAndDataTypesLabs/Labs/DataTypes_Lab_Starter/DataTypes_Lib/TypeConversion.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataTypes_Lib
+{
+    public class TypeConversion
+    {
+        public static short UIntToShort(uint num)
+        {   
+            if(num > short.MaxValue) throw new OverflowException();
+            return (short)num;
+        }
+
+        public static long FloatToLong(float num)
+        {
+            num = MathF.Round(num);
+            return (long)num;
+        }
+    }
+}
