039bd1e2e58d82f247b5b5698dbdaef1cb73cf75
Ameer58 <aimam58@gmail.com>
2022-09-28 01:04:21 +0100

    Methods lab

---
+++ MethodsLab/Labs/Methods_Lab_Starter/Methods_Lib/Methods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Methods_Lib
+{
+    public class Methods
+    {
+        // implement this method so it returns a tuple (weeks, days) 
+        // corresponding to a given number of days
+        public static (int weeks, int days) DaysAndWeeks(int totalDays)
+        {
+            int day = totalDays % 7;
+            int weeks = totalDays / 7;
+            return (weeks, day);
+
+        }
+
+        public static(int square, int cube, double root) PowersRoot(int num)
+        {
+            if (num < 0) throw new ArgumentException();
+            int square = num * num;
+            int cube = square * num;
+            double root = Math.Sqrt(num);
+            return (square,cube,root);
+        }
+
+        public static int RollDice(Random rng)
+        {
+            var num1 = rng.Next(1, 7);
+            var num2 = rng.Next(1, 7);
+            return num1 + num2;
+        }
+
+    }
+}
